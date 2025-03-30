20652da9c3766e90d5a1341c91c6d91e76f9dd47
stegmanfolland299 <stegmanfolland299@gmail.com>
2025-03-30 18:10:02 +0000

    Add 20.js

---
+++ src/20.js
@@ -0,0 +1,9 @@
+// Example Node.js code to demonstrate basic operations like console.log and math
+
+console.log("Hello, World!");
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(3, 5)); // Output: 8
